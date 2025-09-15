4a12b77305a0ae8eb80c6e493b43503ab366c1c7
bunnyreddy2006 <vasaneerajkuamrreddy@gmail.com>
2025-09-15 12:47:25 +0530

    first commit

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-let dog={
-    "name":"cherry",  weight:2.6,
-    color:"black"
-
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
 }
-console.log(typeof(dog))
- console.log(dog)
-
- 
-
-    
-    
+const myCar1 = new Car("jaguar", 2014);
+ const myCar2= new Car("BMW",2017)
+ const myCar3=new Car("KIA",2020)
+console.log(myCar3)
+console.log(typeof(myCar1))
+// class Dog{
+//     constructor(name,color,height){
+//         this.name=name
+//         this.color=color
+//         this.height=height
+//     }
+// }
+// const dog1=new Dog("johnny","blue",2.3)
+// const dog2=new Dog("tom","Yellow",2.2) 
+// const dog3=new Dog("jerry","Red",2.4)
+// console.log(dog1)
+// console.log(typeof(dog1))
+// console.log(dog2)
+// console.log(typeof(dog2))
+// console.log(dog3)
+// console.log(typeof(dog3))
